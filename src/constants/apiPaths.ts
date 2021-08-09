03df05ecd239905c3fcc3ffb3853868156d75b62
Katerina Stolyarova <katerina.stolyarova89@gmail.com>
2021-08-09 13:49:20 +0300

    Chenged import url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://rd0fj4oaj7.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://lst99i43ck.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://rd0fj4oaj7.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://rd0fj4oaj7.execute-api.eu-west-1.amazonaws.com/dev',
 };
