1acb15544e39710dbf64cafd9b5c6e6eb371bb79
Katerina Stolyarova <katerina.stolyarova89@gmail.com>
2021-08-27 13:30:57 +0300

    Fixed delete product function

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
 
 const API_PATHS = {
-  product: 'https://rd0fj4oaj7.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://lst99i43ck.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://rd0fj4oaj7.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://rd0fj4oaj7.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://gn82boemn5.execute-api.eu-west-1.amazonaws.com/dev',
+  order: 'https://gn82boemn5.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://01makovcsc.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://gn82boemn5.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'https://gn82boemn5.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
 export default API_PATHS;
